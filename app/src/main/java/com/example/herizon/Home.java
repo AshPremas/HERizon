1fbf8eb9679133548e9c75683658a352167e0895
AshPremas <160298805+AshPremas@users.noreply.github.com>
2025-02-05 00:17:55 +0530

    fix layout navigations

--- app/src/main/java/com/example/herizon/Home.java
+++ app/src/main/java/com/example/herizon/Home.java
@@ -1,6 +1,9 @@
 package com.example.herizon;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,42 @@ public class Home extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button learningHub = findViewById(R.id.btnLearningHub);
+        learningHub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, Learninghub.class);
+                startActivity(intent);
+            }
+        });
+
+        Button personalSafety = findViewById(R.id.btnPersonalSafety);
+        personalSafety.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, Personalsafety.class);
+                startActivity(intent);
+            }
+        });
+
+        Button community = findViewById(R.id.btnCommunity);
+        community.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, Community.class);
+                startActivity(intent);
+            }
+        });
+
+        Button goBack = findViewById(R.id.btnBack);
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, Login.class);
+                startActivity(intent);
+            }
         });
     }
 }
